2d33078937843c4a4287722baafa8f13edc04461
Vuldy <81223663+Vuldy@users.noreply.github.com>
2022-05-20 06:57:55 +0700

    Time

--- karnaugh/main.c
+++ karnaugh/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "local.h"
+#include <time.h>
 
 int main()
 {
@@ -12,16 +13,28 @@ int main()
     newstack->head = NULL;
 
     int n;
+    clock_t start = clock();
     readFile(oristack, &n);
+    clock_t stop = clock();
+    double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Time elapsed in readFile() ms: %f", elapsed);
 
+clock_t start = clock();
     operate(oristack,          // asal
             newstack,          // hasil
             n-1                  // jumlah aljabar
             );
                     // -> *Stack
+clock_t stop = clock();
+    double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Time elapsed in operate() ms: %f", elapsed);
 
-
+clock_t start = clock();
     writeResult(newstack,
                 n-1
                 );     // -> CUI
+                clock_t stop = clock();
+    double elapsed = (double)(stop - start) * 1000.0 / CLOCKS_PER_SEC;
+    printf("Time elapsed in writeResult() ms: %f", elapsed);
+
 }
